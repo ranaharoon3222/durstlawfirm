8ec5be608ab5f77a8fb17a8b1f4ace089152da29
ranaharoon3222 <ranaharoon3222@gmail.com>
2023-07-21 16:49:37 +0500

    default homepage added

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,50 @@
-import Practise from '@/components/home/prac/Practise';
-import FourBox from '@/components/home/fourBox/FourBox';
-import OurPurpose from '@/components/home/ourPurpose/OurPurpose';
-import Brands from '@/components/brands/Brands';
+import * as prismicH from '@prismicio/helpers';
+import { SliceZone } from '@prismicio/react';
+import { createClient, linkResolver } from '../../prismicio';
+import { components } from '@/slices/index';
+import Head from 'next/head';
+import { isFilled } from '@prismicio/client';
 import FooterCta from '@/commons/FooterCta';
 
-export default function Home() {
+const Home = ({ page, navigation, settings }) => {
+  console.log(page.data);
+
   return (
     <>
-      <Practise />
-      <FourBox />
-      <OurPurpose />
-
-      <Brands />
+      <Head>
+        <title>{page.data.meta_title}</title>
+        {isFilled.keyText(page.data.meta_description) ? (
+          <meta name='description' content={page.data.meta_description} />
+        ) : null}
+      </Head>
+      <SliceZone slices={page.data.slices} components={components} />;
       <FooterCta image={'/images/last-back-img.jpg'}>
         <h1 className='font-medium text-center md:text-left text-white md:text-[3vw]'>
-          Contact The Durst Law Firm{' '}
+          Contact The Durst Law Firm
           <span className='text-primary'> Today. </span>
         </h1>
       </FooterCta>
     </>
   );
+};
+
+export default Home;
+
+export async function getServerSideProps({ params, previewData }) {
+  const client = createClient({ previewData });
+  const page = await client.getByUID('page', 'home');
+  return {
+    props: {
+      page,
+    },
+  };
 }
+
+// export async function getStaticPaths() {
+//   const client = createClient();
+//   const pages = await client.getAllByType('page');
+//   return {
+//     paths: pages.map((page) => prismicH.asLink(page, linkResolver)),
+//     fallback: false,
+//   };
+// }
