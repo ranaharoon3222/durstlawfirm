5bf186d55e41fd4bd00208d9797bf9528aea94f2
ranaharoon3222 <ranaharoon3222@gmail.com>
2023-07-22 16:00:45 +0500

    added tick box

--- src/commons/TickBox.js
+++ src/commons/TickBox.js
@@ -1,18 +1,13 @@
 import Tick from '@/icons/tick';
 import React from 'react';
 
-const TickBox = () => {
+const TickBox = ({ text }) => {
   return (
     <div className='flex '>
       <div className='mt-1 mr-3'>
         <Tick />
       </div>
-      <div>
-        <p>
-          Jump into action immediately, even in the middle of the night if
-          necessary
-        </p>
-      </div>
+      <div>{text}</div>
     </div>
   );
 };
